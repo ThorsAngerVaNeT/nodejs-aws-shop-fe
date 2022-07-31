e09811e8d7855e22cf0d705911f89ed7ce2ed5d7
Vadzim Antonau <vanet@tut.by>
2022-07-31 21:41:52 +0300

    fix: change URL for cart API to https proxy

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://gtgr83serk.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://v7epzfc1vd.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://thorsangervanet-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://hwh9egqhs6.execute-api.eu-west-1.amazonaws.com/api',
 };
 
 export default API_PATHS;
