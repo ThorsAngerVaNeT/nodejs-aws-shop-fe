9bb19a858af2be4ee2fbc1d9cbec82b9938ab320
Vadzim Antonau <vanet@tut.by>
2022-08-14 23:08:51 +0300

    feat: update for BFF

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: 'https://v7epzfc1vd.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://j47r8f8n7a.execute-api.eu-west-1.amazonaws.com',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://gtgr83serk.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://v7epzfc1vd.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://hwh9egqhs6.execute-api.eu-west-1.amazonaws.com/api',
+  bff: 'https://j47r8f8n7a.execute-api.eu-west-1.amazonaws.com',
+  cart: 'https://j47r8f8n7a.execute-api.eu-west-1.amazonaws.com',
 };
 
 export default API_PATHS;
